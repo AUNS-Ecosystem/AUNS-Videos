df9d91b74d579f0b1595353630d888dd27f5a0cd
AUNS Ecosystem <aunseco@gmail.com>
2022-06-05 13:14:28 +0200

    main

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ import './App.css';
 
 const App = () => {
   return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/player" element={<Player />} />
-    </Routes>
+      <div className="appDiv">
+        
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/player" element={<Player />} />
+        </Routes>
+        
+
+      </div>
+      
   )
 };
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { NotificationProvider } from 'web3uikit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="xxxxx" serverUrl="xxxxx">
+    <MoralisProvider appId="DTCjCQ4BcA9rc3pIKfdz5GO5LE17jE8dngMB6AYs" serverUrl="https://2zoknphwfn65.usemoralis.com:2053/server">
       <NotificationProvider>
       <BrowserRouter>
         <App />
